c58a6c4559769600c9d54e80e4e85e713fb3dedd
Jouke Jacobi <jjacobi@bol.com>
2019-12-09 12:09:32 +0100

    2,5 hour daily

---
+++ dags/2_half_hour_dag.py
@@ -0,0 +1,28 @@
+
+from datetime import timedelta
+import datetime
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+
+args = {
+    'owner': 'Airflow',
+    'start_date': datetime.date(2019, 12, 7),
+}
+
+with DAG(
+        dag_id='2_half_hour_dag',
+        default_args=args,
+        schedule_interval=timedelta(minutes=150),
+        dagrun_timeout=timedelta(minutes=60),
+    ) as dag:
+
+    task1 = DummyOperator(task_id='task1')
+    task2 = DummyOperator(task_id='task2')
+    task3 = DummyOperator(task_id='task3')
+    task4 = DummyOperator(task_id='task4')
+    task5 = DummyOperator(task_id='task5')
+
+task1 >> task2 >> task4 >> task5
+task2 >> task3 >> task5
