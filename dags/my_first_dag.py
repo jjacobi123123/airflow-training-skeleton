5c356c66ce048df1ea4c0ff7808bfbc06deba748
Jouke Jacobi <jjacobi@bol.com>
2019-12-09 11:01:06 +0100

    code cleanup first dag

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -3,7 +3,6 @@ from datetime import timedelta
 
 import airflow
 from airflow.models import DAG
-from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 
 args = {
@@ -11,33 +10,18 @@ args = {
     'start_date': airflow.utils.dates.days_ago(2),
 }
 
-dag = DAG(
-    dag_id='my_first_dag',
-    default_args=args,
-    schedule_interval=None,
-    dagrun_timeout=timedelta(minutes=60),
-)
+with DAG(
+        dag_id='my_first_dag',
+        default_args=args,
+        schedule_interval=None,
+        dagrun_timeout=timedelta(minutes=60),
+    ) as dag:
 
-task1 = DummyOperator(
-    task_id='task1',
-    dag=dag,
-)
-task2 = DummyOperator(
-    task_id='task2',
-    dag=dag,
-)
-task3 = DummyOperator(
-    task_id='task3',
-    dag=dag,
-)
-task4 = DummyOperator(
-    task_id='task4',
-    dag=dag,
-)
-task5 = DummyOperator(
-    task_id='task5',
-    dag=dag,
-)
+    task1 = DummyOperator(task_id='task1')
+    task2 = DummyOperator(task_id='task2')
+    task3 = DummyOperator(task_id='task3')
+    task4 = DummyOperator(task_id='task4')
+    task5 = DummyOperator(task_id='task5')
 
 task1 >> task2 >> task4 >> task5
 task2 >> task3 >> task5
