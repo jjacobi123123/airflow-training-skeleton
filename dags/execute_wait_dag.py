a037ee444b476daf906674ec568ed4c2480a3a03
Jouke Jacobi <jjacobi@bol.com>
2019-12-09 13:31:39 +0100

    python and bash operator dag

--- dags/execute_wait_dag.py
+++ dags/execute_wait_dag.py
@@ -16,8 +16,10 @@ args = {
     'start_date': timezone.datetime(2019, 12, 7),
 }
 
+
 def print_execution_date(**context):
     print(context['execution_date'])
+
 
 with DAG(
         dag_id='execute_wait_dag',
@@ -27,11 +29,11 @@ with DAG(
     ) as dag:
 
     print_execution_date_operator = PythonOperator(task_id='print_execution_date',
-                                          python_callable=print_execution_date,
+                                                   python_callable=print_execution_date,
                                                    provide_context=True)
     wait_5 = BashOperator(task_id='wait_5', bash_command="sleep 5")
-    wait_1 = BashOperator(task_id='wait_1', bash_command="sleep 5")
-    wait_10 = BashOperator(task_id='wait_10', bash_command="sleep 5")
+    wait_1 = BashOperator(task_id='wait_1', bash_command="sleep 1")
+    wait_10 = BashOperator(task_id='wait_10', bash_command="sleep 10")
     the_end = DummyOperator(task_id='the_end')
 
 print_execution_date_operator >> wait_1 >> the_end
